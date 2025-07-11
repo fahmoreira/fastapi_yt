e69013364f2246957591336d2adc2584b2745d68
Fabinho <moreira.sva@gmail.com>
2025-07-10 22:00:18 -0300

    teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,12 +6,12 @@ from fastapi_yt.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    '''
+    """
     Esse teste tem 3 etapas (AAA)
     - A: Arrange - Arranjo
     - A: Act - Executa a coisa (o SUT - System Under Test)
     - A: Assert - Granta que A é A
-    '''
+    """
 
     # Arrange
     client = TestClient(app)
