3817930cf277d558bf1f7b4fc1acfbf33b5ac9c4
Fabinho <moreira.sva@gmail.com>
2025-07-13 17:35:14 -0300

    Exercício corrigido

--- fastapi_yt/app.py
+++ fastapi_yt/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fastapi_yt.schemas import Message
 
@@ -10,3 +11,16 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Ola Mundo !'}
+
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo !</h1>
+      </body>
+    </html>"""
